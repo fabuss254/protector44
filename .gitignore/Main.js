c0a53192ec75ec6cc512f46db8817424819348de
fabuss254 <fabuss254@outlook.fr>
2018-06-30 20:44:33 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -115,10 +115,11 @@ bot.on("guildUpdate", guild => {
     guild.fetchAuditLogs({type: "GUILD_UPDATE", limit: 10}).then(audit => {
         var finished = 0;
         audit.entries.forEach(function(v,i){
-             if (v.executor.id != "462669402869989386"){
-                 v.executor.send("Don't try to change the servers settings, this warning has been sent to your violation list, after 10 violations, you'll be **permanently banned!**")
-                 break
-             };
+            if (finished == 0){
+                if (v.executor.id != "462669402869989386"){
+                    v.executor.send("Don't try to change the servers settings, this warning has been sent to your violation list, after 10 violations, you'll be **permanently banned!**")
+                };
+            };
         });
     });
 });
