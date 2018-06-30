3700d4e4e88ef31d62c1540b2205e076ed0f3944
fabuss254 <fabuss254@outlook.fr>
2018-06-30 20:44:50 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -118,6 +118,7 @@ bot.on("guildUpdate", guild => {
             if (finished == 0){
                 if (v.executor.id != "462669402869989386"){
                     v.executor.send("Don't try to change the servers settings, this warning has been sent to your violation list, after 10 violations, you'll be **permanently banned!**")
+                    finished = 1;
                 };
             };
         });
